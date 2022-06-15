9d12ebc8d5db99ac6c59dc8d631f3c9c7196ba61
Andrew Sutjahjo <andrewsutjahjo@gmail.com>
2022-06-15 16:33:36 +0200

    fixup! Add setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import find_packages, setup
 
 import versioneer
 
+
 REPO_ROOT = pathlib.Path(__file__).parent
 
 with open(REPO_ROOT / "README.md", encoding="utf-8") as f:
@@ -14,13 +15,13 @@ REQUIREMENTS = ["pandas"]
 setup(
     name="phoenix-scrape",
     version=versioneer.get_version(),
-    description="Lightweight framework for structured and repeatable model validation",
+    description="Pulling of data from APIs module of peacebuilders' social media analysis toolkit",
     long_description=README,
     long_description_content_type="text/markdown",
     author="datavaluepeople",
     author_email="opensource@datavaluepeople.com",
     url="https://github.com/datavaluepeople/phoenix-scrape",
-    license="GNU",
+    license="GNU General Public License v3 (GPLv3)",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     python_requires=">=3.8",
